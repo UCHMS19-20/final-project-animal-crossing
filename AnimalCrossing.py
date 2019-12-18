c67912ebb71c06e609461d33e35090622ec2bed2
Olivia <oqiu@ucvts.org>
2019-12-18 12:05:16 -0500

    make screen
    made the screen of the game

---
+++ AnimalCrossing.py
@@ -0,0 +1,13 @@
+#import sys and pygame
+import sys
+import pygame
+#import pygame font
+pygame.font.init()
+# initialize pygame
+pygame.init()
+
+# make screen 
+screen = pygame.display.set_mode( (900,900) )
+
+# make color for screen
+white = pygame.Color(0,0,0)
