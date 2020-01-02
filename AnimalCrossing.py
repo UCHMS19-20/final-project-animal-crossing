18b389b48a370cc8c3944b475c5a49a4a7ba1834
Olivia <oqiu@ucvts.org>
2020-01-01 19:07:21 -0500

    Update AnimalCrossing.py
    added some variables

--- AnimalCrossing.py
+++ AnimalCrossing.py
@@ -6,8 +6,32 @@ pygame.font.init()
 # initialize pygame
 pygame.init()
 
-# make screen 
-screen = pygame.display.set_mode( (900,900) )
+# setting the width & height of the screen and the movement speed
+width = 1024   
+height = 768  
+FPS = 60
 
-# make color for screen
-white = pygame.Color(0,0,0)
+#size of each tile
+tile_size = 32
+#dimensions of the grid according to the size of the tiles
+gridwidth = width / tile_size
+gridheight = height / tile_size
+
+ # make screen 
+# screen = pygame.display.set_mode( (900,900) )
+
+# # make color for screen
+# white = pygame.Color(0,0,0)
+
+# # Create a font object
+# font = pygame.font.SysFont("Arial", 50)
+# # Create text using the font
+# text = font.render("Hello", True, (200,0,255))
+
+# # fill the screen with color
+# screen.fill( white )
+# # draw text to screen
+# screen.blit(text, (20, 20) )
+# # update the display
+# pygame.display.flip()
+
