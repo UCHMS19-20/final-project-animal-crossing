ddee9d2e3fa64ec6374a302c4ba8a9bc31656395
Olivia <oqiu@ucvts.org>
2020-01-01 20:28:41 -0500

    Update AnimalCrossing.py
    added main functions

--- AnimalCrossing.py
+++ AnimalCrossing.py
@@ -1,3 +1,5 @@
+conda install -c conda-forge jupyterlab
+
 #import sys and pygame
 import sys
 import pygame
@@ -11,27 +13,112 @@ width = 1024
 height = 768  
 FPS = 60
 
+#title of the game
+title = "Animal Crossing"
+
+#colors of stuff
+background = (0,0,0)
+grey = (40, 40, 40)
+
 #size of each tile
 tile_size = 32
-#dimensions of the grid according to the size of the tiles
+#how many squares are on the screen
 gridwidth = width / tile_size
 gridheight = height / tile_size
 
- # make screen 
-# screen = pygame.display.set_mode( (900,900) )
+#create player class
+class Player(pygame.sprite.Sprite):
+    """player Class"""
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pygame.Surface((tile_size, tile_size))
+        self.image.fill(grey)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+    # size of player based on tilesize 
+    def update(self):
+        self.rect.x = self.x * tile_size
+        self.rect.y = self.y * tile_size
 
-# # make color for screen
-# white = pygame.Color(0,0,0)
+#main loop and functions for the game
+class game():
+    """game class"""
+    #display screen and title
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption(title)
 
-# # Create a font object
-# font = pygame.font.SysFont("Arial", 50)
-# # Create text using the font
-# text = font.render("Hello", True, (200,0,255))
+    def load_data(self):
+        pass
 
-# # fill the screen with color
-# screen.fill( white )
-# # draw text to screen
-# screen.blit(text, (20, 20) )
-# # update the display
-# pygame.display.flip()
+    # initialize all variables and do setup for a new game
+    def new(self):
+        # initialize all variables and do all the setup for a new game
+        self.all_sprites = pygame.sprite.Group()
+        self.walls = pygame.sprite.Group()
+        self.player = Player(self, 10, 10)
+        for x in range(10, 20):
+            Wall(self, x, 5)
+
+    #where the game loops; if self.playing == false, then the game ends
+    def run(self):
+        # game loop - set self.playing = False to end the game
+        self.playing = True
+        while self.playing:
+            self.speed = FPS
+            self.events()
+            self.update()
+            self.draw()
+
+    #ends the game
+    def quit(self):
+            pygame.quit()
+        sys.exit()
+
+    #update sprites during the game
+    def update(self):
+        self.all_sprites.update()
+
+    # draw the grids on the screen
+    def draw_grid:
+        for x in range (0, width, tilesize)
+            pygame.draw.line(self.screen, grey, (x, 0), (x, height))
+        for y in range (0, height, tilesize)
+            pygame.draw.line(self.screen, grey, (0, y), (width, y))
+
+    def draw(self):
+        self.screen.fill(background)
+        self.draw_grid()
+        self.all_sprites.draw(self.screen)
+        pygame.display.flip()
+
+    # allows the player to quit and move
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.quit()
+                if event.key == pygame.K_LEFT:
+                    self.player.move(dx=-1)
+                if event.key == pygame.K_RIGHT:
+                    self.player.move(dx=1)
+                if event.key == pygame.K_UP:
+                    self.player.move(dy=-1)
+                if event.key == pygame.K_DOWN:
+                    self.player.move(dy=1)
+
+    # shows start screen
+    def show_start_screen(self):
+        pass
+
+    #shows go screen
+    def show_go_screen(self):
+        pass
+
 
