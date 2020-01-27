4a1f331ee9cb435e94c1b21a525111ac613e6b8b
Olivia <oqiu@ucvts.org>
2020-01-26 23:07:47 -0500

    Update AnimalCrossing.py

--- AnimalCrossing.py
+++ AnimalCrossing.py
@@ -403,8 +403,9 @@ class Game():
             pygame.event.pump() 
             if pygame.key.get_pressed()[pygame.K_BACKSPACE]: 
                 talk = False #if the user presses spacebar
-                if not talk:
-                    break
+                if pygame.key.get_pressed()[pygame.K_ESCAPE]:
+                    pygame.quit()
+                    sys.exit()
                 # pygame.display.flip() 
                 # clock.tick(60)
                 
